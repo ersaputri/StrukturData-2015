b8e1f05b3c3e4204a44fa5b7937c0d95a0d79678
Ersa Putri Utami <echaputri456@gmail.com>
2015-11-06 14:49:45 +0700

    no message

---
+++ 04-Profil-Test-Awal/Profil.java
@@ -0,0 +1,21 @@
+public class Profil {
+    private String nama="Ersa Putri Utami";
+    private String nim ="1408107010002";
+
+    public Profil(){
+    }
+
+    public Profil (String nama , String nim){
+        this.nama=nama;
+        this.nim=nim;
+    }
+
+    public String getNama (){
+        return nama;
+    }
+
+    public String getNim(){
+        return nim;
+    }
+
+}
